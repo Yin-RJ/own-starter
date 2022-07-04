45dbbafd6fe3c51a8743f1245b72b4555e8769d0
YinRJ <yin_rj@qq.com>
2022-07-04 21:44:55 +0800

    项目初始化

--- src/main/java/com/yinrj/redisspringbootstarter/RedisSpringBootStarterApplication.java
+++ src/main/java/com/yinrj/redisspringbootstarter/RedisSpringBootStarterApplication.java
@@ -1,30 +1,12 @@
 package com.yinrj.redisspringbootstarter;
 
-import com.yinrj.redisspringbootstarter.annotation.EnableOwnAutoImport;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-
-import java.util.Arrays;
 
 @SpringBootApplication
-@EnableOwnAutoImport
 public class RedisSpringBootStarterApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(RedisSpringBootStarterApplication.class, args);
-    }
-
-    @Bean
-    public CommandLineRunner run(ApplicationContext appContext) {
-        return args -> {
-
-            String[] beans = appContext.getBeanDefinitionNames();
-            Arrays.stream(beans).sorted().forEach(System.out::println);
-
-        };
     }
 
 }
